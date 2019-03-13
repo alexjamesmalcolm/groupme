9a02925fb66f54886e9a6ed5aa87c134319410e1
Alexander James Malcolm <ajmalcolm@aep.com>
2019-03-13 11:03:30 -0400

    Initial commit

---
+++ src/main/java/com/alexjamesmalcolm/groupme/GroupmeApplication.java
@@ -0,0 +1,13 @@
+package com.alexjamesmalcolm.groupme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupmeApplication.class, args);
+	}
+
+}
